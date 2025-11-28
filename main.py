fb14d71beebb16b60cce7f44764f4f691783fac2
Matthew <49130090+mw-138@users.noreply.github.com>
2025-11-28 20:51:04 +0000

    feat: perform action on user command

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import sys
+
+from config import COMMANDS
+
+
 def main():
-    pass
+    args = sys.argv
+    arg_len = len(args)
+
+    if arg_len < 2:
+        print("No command provided")
+        return
+
+    command = args[1].lower()
+    found_command = next((c for c in COMMANDS if c.id == command), None)
+    if found_command:
+        found_command.perform_action(args[2:])
+    else:
+        print("command not found")
 
 
 if __name__ == "__main__":
